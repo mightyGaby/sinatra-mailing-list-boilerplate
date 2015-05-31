3658a5a59494ca183ff29a2f600164ff8f17f077
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-30 20:36:31 -0500

    adds validation on server side

--- models/subscriber.rb
+++ models/subscriber.rb
@@ -5,5 +5,5 @@ class Subscriber < ActiveRecord::Base
   # validates_uniqueness_of :username
   # validates_uniqueness_of :email, :message => "this e-mail is already in use"
 
-  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
